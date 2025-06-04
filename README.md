b6bba5b0d61794a7c3590fc708064f5f18011f57
Gabriel Santini <santinigabriel.dev@gmail.com>
2025-06-04 10:06:01 -0300

    feat: add doctor creation form

--- README.md
+++ README.md
@@ -17,6 +17,7 @@
 
 - [x] Sidebar e Route Groups
 - [x] Página de médicos
+- [x] Proteger página de médicos (sutenticação)
 - [] Criação de médicos & NextSafeAction
 - [] Listagem de médicos
 - [] Atualização de médicos
---
+++ src/app/(protected)/doctors/_components/add-doctor-button.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+import { Plus } from "lucide-react";
+import UpsertDoctorForm from "./upsert-doctor-form";
+
+const AddDoctorButton = () => {
+  return (
+    <Dialog>
+      <DialogTrigger>
+        <Button>
+          <Plus />
+          Adicionar médico
+        </Button>
+      </DialogTrigger>
+      <UpsertDoctorForm />
+    </Dialog>
+  );
+};
+
+export default AddDoctorButton;
